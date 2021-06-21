22c2d08cee844222707dce42e315f1c3a7fe060d
Miguel Ángel López <miller13008@gmail.com>
2021-06-21 15:36:54 +0200

    hola

--- Main_C.php
+++ Main_C.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>esto es la prueba 3</title>
+    <title>esto es la prueba 3</title>
+    <title>esto es la prueba 3</title>
+
+
     <title>esto es la prueba 3</title>
     <title>esto es la prueba 3</title>
     <title>esto es la prueba 3</title>
